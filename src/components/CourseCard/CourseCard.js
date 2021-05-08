c31b75cf879b9cab73859e984b7bf44916145b63
osamamammar <osamaammar29@gmail.com>
2021-05-08 18:01:30 +0200

    add styles and fix some issues

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -1,4 +1,7 @@
 import React from "react";
+import courseImg from "../../images/course-img-placeholder.svg";
+import mentorImg from "../../images/mentor-img-placeholder.svg";
+import courseStar from "../../images/course-star.svg";
 
 const CourseCard = () => {
   return (
@@ -7,12 +10,12 @@ const CourseCard = () => {
         <div className="course-card">
           <div className="card-content">
             <div className="course-img">
-              <img src="images/course-img-placeholder.svg" alt="" />
+              <img src={courseImg} alt="" />
             </div>
             <div className="mentor-name-price flex-row">
               <div className="mentor-name flex-row">
                 <div className="mentor-img">
-                  <img src="images/mentor-img-placeholder.svg" alt="" />
+                  <img src={mentorImg} alt="" />
                 </div>
                 <div className="mentor-info">
                   <h2>mentor-x</h2>
@@ -34,7 +37,7 @@ const CourseCard = () => {
               </p>
             </div>
             <div className="course-rate flex-row">
-              <img src="images/course-star.svg" alt="" />
+              <img src={courseStar} alt="" />
               <p>4.8 (231)</p>
             </div>
           </div>
--- src/screens/ExploreScreen/ExploreScreen.js
+++ src/screens/ExploreScreen/ExploreScreen.js
@@ -2,34 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CourseCard from "../../components/CourseCard";
 import ExploreHeader from "../../components/ExploreHeader";
-import Footer from "../../components/Footer/Footer";
+import ExploreSlider from "../../components/ExploreSlider";
+import Filter from "../../components/Filter";
+import Footer from "../../components/Footer";
+import SearchBox from "../../components/SearchBox";
+import "./ExploreScreen.scss";
 
 const ExploreScreen = () => {
   return (
     <>
       <ExploreHeader></ExploreHeader>
+      <SearchBox></SearchBox>
+      <ExploreSlider></ExploreSlider>
+      <Filter></Filter>
       <section class="hr-section-14">
         <div class="container">
           <div class="row">
             <CourseCard></CourseCard>
-            <ul class="list-card">
-              <li class="active">
-                <Link to="">1</Link>
-              </li>
-              <li>
-                <Link href="">2</Link>
-              </li>
-              <li>
-                <Link href="">3</Link>
-              </li>
-              <li>
-                <Link href="">4</Link>
-              </li>
-              <li>
-                <Link href="">5</Link>
-              </li>
-            </ul>
           </div>
+          <ul class="list-card">
+            <li class="active">
+              <Link to="">1</Link>
+            </li>
+            <li>
+              <Link to="">2</Link>
+            </li>
+            <li>
+              <Link to="">3</Link>
+            </li>
+            <li>
+              <Link to="">4</Link>
+            </li>
+            <li>
+              <Link to="">5</Link>
+            </li>
+          </ul>
         </div>
       </section>
       <Footer></Footer>
