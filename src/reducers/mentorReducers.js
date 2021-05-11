3e3878f34a7aaca5bf2d7768c0372bb7dbabdb4c
osamamammar <osamaammar29@gmail.com>
2021-05-11 09:10:37 +0200

    add guardian and mentor reducers

---
+++ src/reducers/mentorReducers.js
@@ -0,0 +1,18 @@
+import {
+  GUARDIAN_REGISTER_FAIL,
+  GUARDIAN_REGISTER_REQUEST,
+  GUARDIAN_REGISTER_SUCCESS,
+} from "../constants/guardianConstants";
+
+export const mentorRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GUARDIAN_REGISTER_REQUEST:
+      return { loading: true };
+    case GUARDIAN_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case GUARDIAN_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
