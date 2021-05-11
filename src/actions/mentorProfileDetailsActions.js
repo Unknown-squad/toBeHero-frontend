34f612cabb2d4db1df4fe884fb20fff09d66cfb4
osamamammar <osamaammar29@gmail.com>
2021-05-11 23:48:41 +0200

    add mentor profile details actions

---
+++ src/actions/mentorProfileDetailsActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import {
+  MENTOR_PROFILE_DETAILS_FAIL,
+  MENTOR_PROFILE_DETAILS_REQUEST,
+  MENTOR_PROFILE_DETAILS_SUCCESS,
+} from "../constants/mentorProfileDetailsConstants";
+
+export const mentorProfileDetails = (mentorId) => async (dispatch) => {
+  try {
+    dispatch({ type: MENTOR_PROFILE_DETAILS_REQUEST });
+    const { data } = await axios.get(
+      `http://localhost:5000//api/v1/mentor/profile/${mentorId}`
+    );
+    dispatch({ type: MENTOR_PROFILE_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: MENTOR_PROFILE_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
