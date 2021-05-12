181a9c4c7f7a3aec8666d8c53b475442b3b56efb
Mohammed Sobhi <m.mohamed.sobhi.98@gmail.com>
2021-05-12 08:03:09 +0200

    Palestine is Freeeeeeeeeeeeee

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,6 +21,26 @@ const Footer = () => {
                 <i className="fab fa-github"></i>
               </a>
             </li>
+            <li>
+              <a href="https://www.github.com/Unknown-squad" target="blank">
+                <img
+                  style={{ width: "20px", marginLeft: "20px" }}
+                  src="https://www.flaticon.com/svg/vstatic/svg/197/197467.svg?token=exp=1620799049~hmac=aec3ee6c11daa478b7607d114a9076b9"
+                  alt="Palestine Flag"
+                />
+                <p
+                  style={{
+                    display: "inline",
+                    fontSize: "14px",
+                    position: "relative",
+                    top: "-3px",
+                    marginLeft: "5px",
+                  }}
+                >
+                  Palestine is Free
+                </p>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
