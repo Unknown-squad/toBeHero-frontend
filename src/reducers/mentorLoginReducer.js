76caafe31a66dd5d5bd20e250d35a8df4fe495f9
osamamammar <osamaammar29@gmail.com>
2021-05-16 00:42:49 +0200

    add mentor login reducers

---
+++ src/reducers/mentorLoginReducer.js
@@ -0,0 +1,21 @@
+import {
+  MENTOR_LOGIN_FAIL,
+  MENTOR_LOGIN_REQUEST,
+  MENTOR_LOGIN_SUCCESS,
+  MENTOR_LOGOUT,
+} from "../constants/mentorLoginConstants";
+
+export const mentorLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case MENTOR_LOGIN_REQUEST:
+      return { loading: true };
+    case MENTOR_LOGIN_SUCCESS:
+      return { loading: false, mentorInfo: action.payload };
+    case MENTOR_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case MENTOR_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
