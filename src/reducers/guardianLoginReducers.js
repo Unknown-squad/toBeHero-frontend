7d53d2cbec203e5eb025fa6ffc86991607a22686
osamamammar <osamaammar29@gmail.com>
2021-05-16 00:36:52 +0200

    add guardian login reducers

---
+++ src/reducers/guardianLoginReducers.js
@@ -0,0 +1,21 @@
+import {
+  GUARDIAN_LOGIN_FAIL,
+  GUARDIAN_LOGIN_REQUEST,
+  GUARDIAN_LOGIN_SUCCESS,
+  GUARDIAN_LOGOUT,
+} from "../constants/guardianLoginConstants";
+
+export const guardianLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GUARDIAN_LOGIN_REQUEST:
+      return { loading: true };
+    case GUARDIAN_LOGIN_SUCCESS:
+      return { loading: false, guardianInfo: action.payload };
+    case GUARDIAN_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case GUARDIAN_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
