7ece103824800963948e3dd4e2dbc176c6123d0e
osamamammar <osamaammar29@gmail.com>
2021-05-24 01:54:38 +0200

    add mentor courses dashboard reducers

---
+++ src/reducers/mentorCoursesDashboardReducers.js
@@ -0,0 +1,32 @@
+import {
+  MENTOR_COURSES_DASHBOARD_FAIL,
+  MENTOR_COURSES_DASHBOARD_REQUEST,
+  MENTOR_COURSES_DASHBOARD_SUCCESS,
+} from "../constants/mentorCoursesDashboardConstants";
+
+const initialState = {
+  data: {
+    Kind: "",
+    count: "",
+    items: [
+      {
+        _id: "",
+        title: "",
+        description: "",
+        picture: "",
+      },
+    ],
+  },
+};
+export const mentorCoursesDashboardReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case MENTOR_COURSES_DASHBOARD_REQUEST:
+      return { loading: true };
+    case MENTOR_COURSES_DASHBOARD_SUCCESS:
+      return { loading: false, mentor: action.payload.data };
+    case MENTOR_COURSES_DASHBOARD_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
