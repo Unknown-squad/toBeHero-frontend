e003b5e8886533fdc204153b3080f05eb41beb25
osamamammar <osamaammar29@gmail.com>
2021-06-18 20:32:30 +0200

    add mentor status update availability

---
+++ src/actions/mentorStatusUpdateAvailabilityActions.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {
+  MENTOR_STATUS_FAIL,
+  MENTOR_STATUS_REQUEST,
+  MENTOR_STATUS_SUCCESS,
+} from "../constants/mentorStatusAvailabilityConstants";
+
+export const mentorStatusUpdateAvailabilityActions = () => async (dispatch) => {
+  try {
+    dispatch({ type: MENTOR_STATUS_REQUEST });
+    const { data } = await axios.put(
+      "http://localhost:5000/api/v1/mentor/availability",
+      { withCredentials: true }
+    );
+
+    dispatch({
+      type: MENTOR_STATUS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    let err = "";
+    if (error.response) {
+      // Request made and server responded
+      err = error.response.data.error.message;
+    } else if (error.request) {
+      // The request was made but no response was received
+      err = error.request;
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      err = error.message;
+    }
+    dispatch({
+      type: MENTOR_STATUS_FAIL,
+      payload: err,
+    });
+  }
+};
