1f6407bbfafccf516c85f6dbe6a5c1dd5e55699f
osamamammar <osamaammar29@gmail.com>
2021-06-19 16:13:53 +0200

    add mentor cancel appointment actions and constants

---
+++ src/actions/mentorCancelAppointmentActions.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+import {
+  MENTOR_CANCEL_APPOINTMENT_FAIL,
+  MENTOR_CANCEL_APPOINTMENT_REQUEST,
+  MENTOR_CANCEL_APPOINTMENT_SUCCESS,
+} from "../constants/mentorCancelAppointmentConstants";
+
+export const mentorCancelAppointmentActions =
+  (subscriptionId, appointmentId) => async (dispatch) => {
+    try {
+      dispatch({ type: MENTOR_CANCEL_APPOINTMENT_REQUEST });
+      const { data } = await axios.put(
+        `http://localhost:5000/api/v1/mentor/subscription/${subscriptionId}/appointment/${appointmentId}/cancel`,
+        {},
+        { withCredentials: true }
+      );
+
+      dispatch({
+        type: MENTOR_CANCEL_APPOINTMENT_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      let err = "";
+      if (error.response) {
+        // Request made and server responded
+        err = error.response.data.error.message;
+      } else if (error.request) {
+        // The request was made but no response was received
+        err = error.request;
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        err = error.message;
+      }
+      dispatch({
+        type: MENTOR_CANCEL_APPOINTMENT_FAIL,
+        payload: err,
+      });
+    }
+  };
