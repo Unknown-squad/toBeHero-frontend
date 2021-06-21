fd8afd934119acd43b1fd9cdcbf50e7a00da846f
osamamammar <osamaammar29@gmail.com>
2021-06-21 03:53:36 +0200

    add get subscriptions for child actions

---
+++ src/actions/getSubscriptionsForChildActions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export const getSubscriptionsForChildActions =
+  (childId) => async (dispatch) => {
+    try {
+      dispatch({ type: GET_SUBSCRIPTIONS_FOR_CHILD_REQUEST });
+
+      const { data } = await axios.get(
+        `http://localhost:5000/api/v1/guardian/child-subscription/${childId}`,
+        { withCredentials: true }
+      );
+
+      dispatch({
+        type: GET_SUBSCRIPTIONS_FOR_CHILD_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      let err = "";
+      if (error.response) {
+        // Request made and server responded
+        err = error.response.data.error.message;
+      } else if (error.request) {
+        // The request was made but no response was received
+        err = error.request;
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        err = error.message;
+      }
+      dispatch({
+        type: GET_SUBSCRIPTIONS_FOR_CHILD_FAIL,
+        payload: err,
+      });
+    }
+  };
