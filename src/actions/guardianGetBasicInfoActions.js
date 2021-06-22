d0adb0d3a1d8eba6b977300a3d01f53b10544ddd
osamamammar <osamaammar29@gmail.com>
2021-06-22 21:30:05 +0200

    add guardian get basic info actions

---
+++ src/actions/guardianGetBasicInfoActions.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const guardianGetBasicInfoActions = () => async (dispatch) => {
+  try {
+    dispatch({ type: GUARDIAN_GET_BASIC_INFO_REQUEST });
+
+    const { data } = await axios.get(
+      `http://localhost:5000/api/v1/guardian/basic-info`,
+      { withCredentials: true }
+    );
+
+    dispatch({
+      type: GUARDIAN_GET_BASIC_INFO_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    let err = "";
+    if (error.response) {
+      // Request made and server responded
+      err = error.response.data.error.message;
+    } else if (error.request) {
+      // The request was made but no response was received
+      err = error.request;
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      err = error.message;
+    }
+    dispatch({
+      type: GUARDIAN_GET_BASIC_INFO_FAIL,
+      payload: err,
+    });
+  }
+};
