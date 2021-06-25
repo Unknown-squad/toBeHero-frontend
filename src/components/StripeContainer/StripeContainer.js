237128b9808ae8182bd31811a108b11405a9a5f3
osamamammar <osamaammar29@gmail.com>
2021-06-25 17:25:04 +0200

    add stripe container component

---
+++ src/components/StripeContainer/StripeContainer.js
@@ -0,0 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import PaymentForm from "./PaymentForm";
+
+const PUBLIC_KEY =
+  "pk_test_51InwK8DlMEFhQMv2dZ2ccm6uTXM16lwUhMA6w7klu6ymp0Cu4JO1FE6DiADhLng0aqmcCcy9TBzfjop6bXVkl3nk00hqa1SYPf";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+export default function StripeContainer() {
+  return (
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm />
+    </Elements>
+  );
+}
