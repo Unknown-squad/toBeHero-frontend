e7149876c3151a33b945150eccfeac71e697f00b
osamamammar <osamaammar29@gmail.com>
2021-07-07 06:07:10 +0200

    remove repeated code Aos animation for landing page

--- src/screens/LandingScreen/LandingScreen.js
+++ src/screens/LandingScreen/LandingScreen.js
@@ -18,10 +18,9 @@ import Headroom from "react-headroom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { default as locales } from "./locales.json";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import Slider from "react-slick";
 import "./sliderLanding.scss";
+import AOS from "../../components/AOS";
 const LandingScreen = () => {
   let languageStoredInLocalStorage = localStorage.getItem("language");
   const [language, setLanguage] = useState(
@@ -39,26 +38,6 @@ const LandingScreen = () => {
     document.body.dir = content.dir;
   }, [content.dir]);
 
-  AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: "aos-init", // class applied after initialization
-    animatedClassName: "aos-animate", // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 800, // values from 0 to 3000, with step 50ms
-    easing: "ease", // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-  });
   function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
@@ -132,6 +111,7 @@ const LandingScreen = () => {
       },
     ],
   };
+  AOS.init({ duration: 800 });
   return (
     <>
       <Meta></Meta>
